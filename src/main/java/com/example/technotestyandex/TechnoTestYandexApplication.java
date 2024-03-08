97731b395fdc6820f4f84d342f981f6575ddcf82
Eljan Najafov <eljan.najafov02@mail.ru>
2024-03-08 13:32:10 +0400

    techno code release

---
+++ src/main/java/com/example/technotestyandex/TechnoTestYandexApplication.java
@@ -0,0 +1,13 @@
+package com.example.technotestyandex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoTestYandexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnoTestYandexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/technotestyandex/TechnoTestYandexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technotestyandex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoTestYandexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
